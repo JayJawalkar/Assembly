23e430f6a765c95b7c2350268c330b2256c5c438
unknown <jawalkarjay7@gmail.com>
2024-03-13 12:43:48 +0530

    first commit

---
+++ asc.asm
@@ -0,0 +1,31 @@
+data segment
+num db 12,5,3,8
+data ends 
+code segment
+assume cs:code,ds:data
+start:
+MOv ax,data
+Mov ds,ax
+Mov bx,3 
+t1:
+Mov cx,3
+Mov si,offset num
+t2:
+Mov al,[si]
+CMP al,[si+1]
+jnc t3
+inc si
+loop t2
+jmp t4
+t3:
+XCHG al,[si+1]
+xchg al,[si]
+inc si
+loop t2
+t4:
+dec bx
+jc t1
+Mov ah,4ch
+INT 21h
+code ends
+End start
